f211998bf5bb6e2d7afdae81a788cd9a434dc85f
dearolaf <albert.jin222@gmail.com>
2026-03-26 07:44:03 -0500

    fixed issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1268210288"
+    serviceWorkerVersion: "2731732246"
   }
 });
